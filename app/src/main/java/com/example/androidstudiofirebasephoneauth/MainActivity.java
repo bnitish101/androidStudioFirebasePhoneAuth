038d15ae3d97969ecf1c362cb999f53fda7fa006
Nitish <bnitish101@gmail.com>
2021-10-31 20:53:05 +0530

    onclick redirect to mange otp activity

--- app/src/main/java/com/example/androidstudiofirebasephoneauth/MainActivity.java
+++ app/src/main/java/com/example/androidstudiofirebasephoneauth/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.androidstudiofirebasephoneauth;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
         et_mobile = (EditText) findViewById(R.id.et_mobile);
         btn_getOtp = (Button) findViewById(R.id.btn_getOtp);
 
+        btn_getOtp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,manageOPT.class);
+                intent.putExtra("mobile", ccp.getFullNumberWithPlus().replace(" ", ""));
+                startActivity(intent);
+            }
+        });
     }
 
 }
