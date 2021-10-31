abb36c6cd8f9995a6709de6b10295145f64b6e45
Nitish <bnitish101@gmail.com>
2021-10-31 23:42:01 +0530

    log out button function written in dashboard activity

--- app/src/main/java/com/example/androidstudiofirebasephoneauth/dashboard.java
+++ app/src/main/java/com/example/androidstudiofirebasephoneauth/dashboard.java
@@ -3,12 +3,27 @@ package com.example.androidstudiofirebasephoneauth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class dashboard extends AppCompatActivity {
 
+    Button btn_logOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        btn_logOut = (Button) findViewById(R.id.btn_logOut);
+
+        btn_logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                finish();
+            }
+        });
     }
 }
