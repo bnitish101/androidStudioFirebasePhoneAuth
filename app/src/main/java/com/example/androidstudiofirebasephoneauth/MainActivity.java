ee0e79109af41519c529e7951dce511d8989e6b0
Nitish <bnitish101@gmail.com>
2021-10-31 20:43:01 +0530

    casting is done for main activity

--- app/src/main/java/com/example/androidstudiofirebasephoneauth/MainActivity.java
+++ app/src/main/java/com/example/androidstudiofirebasephoneauth/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.androidstudiofirebasephoneauth;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
 
+    CountryCodePicker ccp;
+    EditText et_mobile;
+    Button btn_getOtp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ccp = (CountryCodePicker) findViewById(R.id.ccp);
+        ccp.registerCarrierNumberEditText(et_mobile);
+
+        et_mobile = (EditText) findViewById(R.id.et_mobile);
+        btn_getOtp = (Button) findViewById(R.id.btn_getOtp);
+
     }
+
 }
