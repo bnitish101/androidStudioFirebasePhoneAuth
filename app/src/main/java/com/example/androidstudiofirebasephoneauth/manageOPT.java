8cc44a08617ad659c6e48476ed0cd048a1c42176
Nitish <bnitish101@gmail.com>
2021-10-31 20:33:05 +0530

    dashboard activity created from xml design

---
+++ app/src/main/java/com/example/androidstudiofirebasephoneauth/manageOPT.java
@@ -0,0 +1,14 @@
+package com.example.androidstudiofirebasephoneauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class manageOPT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_opt);
+    }
+}
